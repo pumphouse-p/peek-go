454a4f67d2882561511e67c5e1e93f5e746bb5e6
Devin Parrish <devin.parrish@gmail.com>
2021-09-15 17:37:58 +0000

    Tidy net package

--- pkg/net/api.go
+++ pkg/net/api.go
@@ -1,19 +1,31 @@
 package net
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/pumphouse-p/peek-go/pkg/apiutils"
 )
 
+type IPAddress struct {
+	IP   string `json:"ip"`
+	Zone string `json:"zone,omitempty"`
+}
+
+type NetworkInterface struct {
+	Index        int         `json:"index"`
+	MTU          int         `json:"mtu"`
+	Name         string      `json:"name"`
+	HardwareAddr string      `json:"hwaddr"`
+	Flags        *net.Flags  `json:"flags,omitempty"`
+	IPAddress    []IPAddress `json:"ipaddr"`
+}
+
 type NetStatus struct {
-	Hostname   string                    `json:"hostname"`
-	Interfaces map[int]map[string]string `json:"interfaces"`
+	Hostname   string             `json:"hostname"`
+	Interfaces []NetworkInterface `json:"interfaces,omitempty"`
 }
 
 type Net struct{}
@@ -22,42 +34,51 @@ func New() *Net {
 	return &Net{}
 }
 
-func CollectHostname() (h string) {
+func GetHostname() (h string) {
 	h, _ = os.Hostname()
 	return h
 }
 
-func CollectNetInfo() map[int]map[string]string {
-	netinfo := make(map[int]map[string]string)
+func GetInterfaces() []NetworkInterface {
 
 	interfaces, err := net.Interfaces()
+	var sanitized []NetworkInterface
+
 	if err != nil {
 		log.Printf("Could not retrieve network interfaces")
 	}
 
+	log.Printf("Got %d interfaces", len(interfaces))
+
 	for _, e := range interfaces {
 		log.Printf("Collecting info for interface: %v", e.Name)
-		iface := make(map[string]string)
-		iface["mtu"] = strconv.Itoa(e.MTU)
-		iface["name"] = e.Name
-		iface["mac-address"] = e.HardwareAddr.String()
+		iface := NetworkInterface{}
+		iface.MTU = e.MTU
+		iface.Name = e.Name
+		iface.Index = e.Index
+		iface.HardwareAddr = e.HardwareAddr.String()
+
 		log.Printf("Collecting IP addresses")
 		addr, _ := e.Addrs()
-		for i, a := range addr {
-			k := fmt.Sprintf("ip-%d", i)
-			iface[k] = a.String()
+
+		for _, a := range addr {
+			ip := IPAddress{
+				IP: a.String(),
+			}
+			iface.IPAddress = append(iface.IPAddress, ip)
 		}
-		netinfo[e.Index] = iface
+
+		sanitized = append(sanitized, iface)
 	}
 
-	return netinfo
+	return sanitized
 }
 
 func (n *Net) APIGet(w http.ResponseWriter, r *http.Request) {
 	status := NetStatus{}
 
-	status.Hostname = CollectHostname()
-	status.Interfaces = CollectNetInfo()
+	status.Hostname = GetHostname()
+	status.Interfaces = GetInterfaces()
 
 	apiutils.ServeJSON(w, status)
 }
